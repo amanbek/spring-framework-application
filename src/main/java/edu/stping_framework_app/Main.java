b04261325bb5f8a426a64c9f2a601739ceabf976
Aibek Dandaev <amanbekazizuulu@gmail.com>
2023-10-28 08:13:49 +0600

    Destroyer and Initializer methods were added

--- src/main/java/edu/stping_framework_app/Main.java
+++ src/main/java/edu/stping_framework_app/Main.java
@@ -3,16 +3,15 @@ package edu.stping_framework_app;
 import org.springframework.beans.BeansException;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import edu.stping_framework_app.entities.MusicPlayer;
+import edu.stping_framework_app.music.ClassicalMusic;
 
 public class Main {
 
 	public static void main (String [] args) {
 		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext ("apllicationcontext.xml")) {
 
-			MusicPlayer player = context.getBean ("music-player-1", MusicPlayer.class);
-			player.getPlayerInfo ();
-			player.playPlayList ();
+			var classicalMusic = context.getBean ("classical-music-2", ClassicalMusic.class);
+			System.out.println (classicalMusic.getSong ());		
 
 		} catch (BeansException e) {
 			e.printStackTrace ();
