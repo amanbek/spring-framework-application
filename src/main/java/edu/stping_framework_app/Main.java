ce29f1842a08a7343a35ec38bf7346949457c9e5
Aibek Dandaev <amanbekazizuulu@gmail.com>
2023-10-28 16:52:56 +0600

    Added factory-method injection functionality

--- src/main/java/edu/stping_framework_app/Main.java
+++ src/main/java/edu/stping_framework_app/Main.java
@@ -10,9 +10,9 @@ public class Main {
 	public static void main (String [] args) {
 		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext ("apllicationcontext.xml")) {
 
-			var classicalMusic = context.getBean ("classical-music-2", ClassicalMusic.class);
+			ClassicalMusic classicalMusic = context.getBean ("classical-music-2", ClassicalMusic.class);
 			System.out.println (classicalMusic.getSong ());		
-
+			
 		} catch (BeansException e) {
 			e.printStackTrace ();
 		}
--- src/main/java/edu/stping_framework_app/music/ClassicalMusic.java
+++ src/main/java/edu/stping_framework_app/music/ClassicalMusic.java
@@ -1,7 +1,10 @@
 package edu.stping_framework_app.music;
 
 public class ClassicalMusic implements Music {
-
+	
+	private ClassicalMusic() {
+	}
+	
 	@ Override
 	public String getSong () {
 		return "Liszt - Hungarian Rhapsody No. 2";
@@ -13,5 +16,9 @@ public class ClassicalMusic implements Music {
 	
 	public void doMyDestroy() {
 		System.out.println ("Bean Destroyed successfully!");
+	}
+	
+	public static ClassicalMusic getInstance() {
+		return new ClassicalMusic ();
 	}
 }
