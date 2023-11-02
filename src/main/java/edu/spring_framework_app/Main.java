b299bc7d4f13d010cb9d0106577497103429c2ef
Aibek Dandaev <amanbekazizuulu@gmail.com>
2023-11-02 09:20:39 +0600

    Functionality for creating a playlist using beans through the @Component annotations has been added.

--- src/main/java/edu/spring_framework_app/Main.java
+++ src/main/java/edu/spring_framework_app/Main.java
@@ -15,17 +15,15 @@ public class Main {
 	public static void main (String [] args) {
 		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext ("apllicationcontext.xml")) {
 
-			 MusicPlayer musicPlayer = new MusicPlayer (
-			 context.getBean ("pop-music", PopMusic.class),
-			 context.getBean ("rock-music", RockMusic.class),
-			 context.getBean ("classical-music", ClassicalMusic.class));
-			
-			 musicPlayer.playPlayList ();
+			MusicPlayer musicPlayer = new MusicPlayer (
+														context.getBean ("pop-music", PopMusic.class),
+														context.getBean ("rock-music", RockMusic.class),
+														context.getBean ("classical-music", ClassicalMusic.class));
+
+			musicPlayer.playPlayList ();
 
 			MusicPlayer musicPlayer_1 = context .getBean ("music-player", MusicPlayer.class)
-												.addSongToPlayList (context.getBean ("pop-music", PopMusic.class), 
-												                    context.getBean ("rock-music", RockMusic.class), 
-												                    context.getBean ("classical-music", ClassicalMusic.class));
+												.addSongToPlayList (context.getBean ("pop-music", PopMusic.class), context.getBean ("rock-music", RockMusic.class), context.getBean ("classical-music", ClassicalMusic.class));
 
 			musicPlayer_1.playPlayList ();
 		} catch (BeansException e) {
